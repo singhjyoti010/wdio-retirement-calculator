836c25953f0468011d5e7520faa91195db5b6fea
Jyoti Singh <singhjyoti010@outlook.com>
2024-11-13 00:49:43 +0530

    created constants to store userinputs separate

---
+++ test/helpers/constants.ts
@@ -0,0 +1,12 @@
+export const userInput = {
+    CurrentAge : 40,
+    RetirementAge : 68,
+    CurrentAnnualIncome: 100000,
+    SpouseAnnualIncome: 75000,
+    CurrentRetirementSavings: 500000,
+    CurrentRetirementContribution: 10,
+    AnnualRetirementContributionIncrease: 25,
+    SocialSecurityIncome: 'Yes',
+    RelationshipStatus: 'Married',
+    SocialSecurityOverride: 4000,
+}
