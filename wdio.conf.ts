02924aaf767d77c10d9b61c79ac342bfff307d19
Jyoti Singh <singhjyoti010@outlook.com>
2024-11-13 00:49:14 +0530

    added default timeout in conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -90,7 +90,7 @@ export const config: WebdriverIO.Config = {
     baseUrl: 'https://www.securian.com',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 20000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
